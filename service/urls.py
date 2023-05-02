0cca60a54c91715ad17c092928fd2b491e34a43d
nkosyrev <shutnik1986@gmail.com>
2023-05-01 20:22:52 -0700

    add: controler page_num

--- service/urls.py
+++ service/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 
-from service.views import index
+from service.views import index, page
+
+# register_converter(converts.CheckParam, "dif_type")
 
 urlpatterns = [
     path('service/', index),
+    re_path(r'^service/(?P<page_num>[0-9]{3})/$', page),
 ]
+
--- service/views.py
+++ service/views.py
@@ -2,3 +2,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Hello World')
+
+def page(request, page_num):
+    return HttpResponse(f'Page {page_num}')
+
