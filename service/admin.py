657ab829cbc086c8c21e64667b9c42735835d7a4
nkosyrev <shutnik1986@gmail.com>
2023-05-01 21:18:32 -0700

    add: learn admin panel

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Bin
+
+# admin.site.register(Product)
+admin.site.register(Bin)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'id', 'cost']
+    fields = ['id', 'title']
+    list_filter = ['cost']
+    search_fields = ['title']
+
+# admin.site.register(Product, ProductAdmin)
--- service/models.py
+++ service/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    
+    id = models.IntegerField(primary_key=True)
+    cost = models.CharField(max_length=200, null=True)
+    title = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return self.title
+
+class Bin(models.Model):
+    
+    id = models.IntegerField(primary_key=True)
+    cost = models.CharField(max_length=200, null=True)
+    title = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return self.title
